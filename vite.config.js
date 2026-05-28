e3a23aa0cf8d042f23fb5466d0f69919d4c35d71
wcsmcode <806nguyentuankiet@gmail.com>
2026-05-28 10:29:18 +0700

    vercel host fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        auth: resolve(__dirname, 'app/auth.html'),
+        home: resolve(__dirname, 'app/home.html'),
+        guest: resolve(__dirname, 'guest/guest.html'),
+      },
+    },
+  },
 })
